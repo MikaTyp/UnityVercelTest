0d9e52a53eb1d0c42aedf22edd0e0988031411de
MikaTyp <mika.tenokoski@typingmaster.com>
2023-10-13 13:39:54 +0300

    gameci

---
+++ .github/workflows/edit_packages.sh
@@ -0,0 +1,11 @@
+# #!/usr/bin/env bash
+
+# sudo sed -i "s|git@github.com:TypingTrainer2/text-color-manager-package.git|file:/github/workspace/text-color-manager-package|" Packages/manifest.json
+# sudo sed -i "s|git@github.com:TypingTrainer2/tween-package.git|file:/github/workspace/tween-package|" Packages/manifest.json
+# sudo sed -i "s|git@github.com:TypingTrainer2/text-data-manager-package.git|file:/github/workspace/text-data-manager-package|" Packages/manifest.json
+# sudo sed -i "s|git@github.com:TypingTrainer2/browser-api-package.git|file:/github/workspace/browser-api-package|" Packages/manifest.json
+# sudo sed -i "s|git@github.com:TypingTrainer2/chunk-tools-package.git|file:/github/workspace/chunk-tools-package|" Packages/manifest.json
+# sudo sed -i "s|git@github.com:TypingTrainer2/tools-package.git|file:/github/workspace/tools-package|" Packages/manifest.json
+# sudo sed -i "s|git@github.com:TypingTrainer2/trace-text-package.git|file:/github/workspace/trace-text-package|" Packages/manifest.json
+# sudo sed -i "s|git@github.com:TypingTrainer2/game-event-package.git|file:/github/workspace/game-event-package|" Packages/manifest.json
+# sudo sed -i "s|git@github.com:TypingTrainer2/result-screen-package.git|file:/github/workspace/result-screen-package|" Packages/manifest.json
