cdb19e24e51945a73d36ae0d9cce59b1a0d62aae
MikaTyp <mika.tenokoski@typingmaster.com>
2023-10-17 12:40:57 +0300

    testing merging

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Test 3");
+        Debug.Log("Test 4");
     }
 
     // Update is called once per frame
