7c2ba0cdef645235559c7aa2303b4d94c7e4a3de
MikaTyp <mika.tenokoski@typingmaster.com>
2023-10-17 12:49:20 +0300

    testing pr branch synch example

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Test 4");
+        Debug.Log("Test 5");
     }
 
     // Update is called once per frame
