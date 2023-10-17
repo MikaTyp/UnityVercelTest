a149f164d2df27cfb15baaa02bd803f691ba0e23
MikaTyp <mika.tenokoski@typingmaster.com>
2023-10-17 11:38:18 +0300

    test 3

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Test 2");
+        Debug.Log("Test 3");
     }
 
     // Update is called once per frame
