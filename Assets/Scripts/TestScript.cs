0969700ebd93f2f94868624180fb5356684cdcfd
MikaTyp <mika.tenokoski@typingmaster.com>
2023-10-17 11:17:30 +0300

    removed extra deploy

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Test 1");
+        Debug.Log("Test 2");
     }
 
     // Update is called once per frame
